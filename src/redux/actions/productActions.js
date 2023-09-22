e2ab21a1fcb98babf63bf614d153d44a41aad9a8
bobosii <day.emir@hotmail.com>
2023-09-22 14:52:10 +0300

    basic error handling

--- src/redux/actions/productActions.js
+++ src/redux/actions/productActions.js
@@ -4,6 +4,52 @@ export function getProductSuccess(products) {
   return { type: actionTypes.GET_PRODUCTS_SUCCESS, payload: products };
 }
 
+export function createProductSuccess(product) {
+  return { action: actionTypes.CREATE_PRODUCT_SUCCESS, payload: product };
+}
+
+export function updateProductSuccess(product) {
+  return { action: actionTypes.UPDATE_PRODUCT_SUCCESS, payload: product };
+}
+
+export function saveProductApi(product) {
+  return fetch("http://localhost:3000/products/" + (product.id || ""), {
+    method: product.id ? "PUT" : "POST",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify(), // datayı burada yolluyoruz
+  })
+    .then(handleResponse)
+    .catch(handleError); // eğer id gönderilmişse güncelle eğer id gönderilmediyse güncellemene gerek yok boş devam et
+}
+
+export function saveProduct(product) {
+  return function (dispatch) {
+    return saveProductApi(product)
+      .then((savedProduct) => {
+        product.id
+          ? dispatch(updateProductSuccess(savedProduct))
+          : dispatch(createProductSuccess(savedProduct));
+      })
+      .catch((error) => {
+        throw error;
+      });
+  };
+}
+
+export async function handleResponse(response) {
+  if (response.ok) {
+    return response.json();
+  }
+
+  const error = await response.text();
+  throw new Error(error);
+}
+
+export function handleError(error) {
+  console.error("Bir hata oluştu");
+  throw error;
+}
+
 export function getProducts(categoryId) {
   return function (dispatch) {
     let url = "http://localhost:3000/products";

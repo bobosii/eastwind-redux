0804c216c95579cdb4980ca8d6c2dea40fdc9408
bobosii <day.emir@hotmail.com>
2023-09-22 20:21:03 +0300

    addOrUpdate

--- src/components/root/App.js
+++ src/components/root/App.js
@@ -4,15 +4,20 @@ import Navi from "../navi/Navi";
 import Dashboard from "../navi/Dashboard";
 import { Route, Routes } from "react-router-dom";
 import CartDetail from "../cart/CartDetail";
+import AddOrUpdate from "../products/AddOrUpdate";
 
 function App() {
   return (
     <Container>
       <Navi />
       <Routes>
-        <Route path="/" element={<Dashboard></Dashboard>}/>
-        <Route path="/product" element={<Dashboard></Dashboard>}/>
-        <Route path="/cart" element={<CartDetail></CartDetail>}/>
+        <Route path="/" element={<Dashboard></Dashboard>} />
+        <Route path="/product" element={<Dashboard></Dashboard>} />
+        <Route path="/cart" element={<CartDetail></CartDetail>} />
+        <Route
+          path="/saveproduct/:productId"
+          element={<AddOrUpdate></AddOrUpdate>}
+        />
       </Routes>
     </Container>
   );
